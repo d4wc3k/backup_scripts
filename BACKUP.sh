6be1c5328d049a63f66ad596c1de36c5ac8cd0a4
d4wc4k <115703675+d4wc3k@users.noreply.github.com>
2025-07-23 20:03:10 +0200

    Add files via upload

--- BACKUP.sh
+++ BACKUP.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
+##############################################################################################################################################################################
 #
 ## DISKs devices names found in "/dev/disk/by-id/*"
 MAIN_DISK_NAME="nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M910268J"
 # SECOND_DISK_NAME="nvme-Samsung_SSD_980_1TB_S649NJ0R214022Y"
 #
 ## Partitions for backup
+#
 declare -A PARTITIONS=(["EFI"]="vfat" ["WINMSR"]="raw" ["WINOS"]="ntfs" ["WINREC"]="ntfs" ["WINDATA"]="ntfs" )
 #
-## Get password for encryption ( optional )
-# PASS_CRED=$(gpg --quiet --decrypt credentials.gpg)
-#
-## LUKS encrypted partition labels ( optional )
-#
-# MAIN_CRYPT_LABEL="rcrypt"
-# SECOND_CRYPT_LABEL="dcrypt"
-# LVM_DECRYPTED_NAME="lvm_unencrypted"
-# VG_NAME="vg"
-#
+##############################################################################################################################################################################
 ## MAIN
-################################################################################################################
+#
 for PART in "${!PARTITIONS[@]}"; 
 do
+	echo "################################################################################################################"
 	LABEL="${PART}"
-	echo "Current partition label: ${LABEL}"
 	FILE_SYSTEM="${PARTITIONS[$LABEL]}"
-	echo "Current partition filesystem: ${FILE_SYSTEM}"
 	echo "Processing partition with ${LABEL} label and ${FILE_SYSTEM} filesystem"
 	PARTLABEL_DEV="/dev/disk/by-partlabel/${LABEL}"
 	if [[ -h "${PARTLABEL_DEV}" ]];
@@ -42,54 +34,38 @@ do
 			continue
 		fi
 	fi
-	## 7z compression
-	# FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.7z"
 	#
 	## gzip compression(gzip/pigz)
+	#
 	FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.gz"
 	#
 	if [ "${FILE_SYSTEM}" = "raw" ];
 	then
-		## 7z compression
-		# echo "Creation raw image for partition with  ${LABEL} label with 7zip compression tool"
-		# partclone.dd -N -z 10485760  -s "${DEV_PATH}" --output - | 7z a -t7z "${FILE_NAME}" -si -m0=lzma2 -mx=5 -mfb=64 -md512m -mmt8
-		#
-		## 7z compression and encryption
-		# echo "Creation raw image for partition with  ${LABEL} label with 7zip (encrypted) compression tool"
-		# partclone.dd -N -z 10485760  -s "${DEV_PATH}" --output - | 7z a -t7z -mhe=on -p"${PASS_CRED}" "${FILE_NAME}" -si -m0=lzma2 -mx=5 -mfb=64 -md512m -mmt8
-		#
 		## gzip compression
+		#
 		# echo "Creation raw image for partition with  ${LABEL} label with gz compression tool"
 		# partclone.dd -N -z 10485760  -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
 		#
 		## gzip with pigz compression
+		#
 		echo "Creation raw image for partition with  ${LABEL} label with pigz compression tool"
 		partclone.dd -N -z 10485760  -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
 		#
 	else
-		## 7z compression
-		# echo "Creation partclone image for partition with  ${LABEL} label with 7zip compression tool"
-		# partclone.$FILE_SYSTEM -N -z 10485760  -c -s "${DEV_PATH}" --output - | 7z a -t7z  "${FILE_NAME}" -si -m0=lzma2 -mx=5 -mfb=64 -md512m -mmt8
-		#
-		## 7z compression and encryption
-		# echo "Creation partclone image for partition with  ${LABEL} label with 7zip (encrypted) compression tool"
-		# partclone.$FILE_SYSTEM -N -z 10485760  -c -s "${DEV_PATH}" --output - | 7z a -t7z -mhe=on -p"${PASS_CRED}" "${FILE_NAME}" -si -m0=lzma2 -mx=5 -mfb=64 -md512m -mmt8
-		#
 		## gzip compression
+		#
 		# echo "Creation partclone image for partition with  ${LABEL} label with gz compression tool"
-		## partclone.$FILE_SYSTEM -N -z 10485760  -c -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
+		# partclone.$FILE_SYSTEM -N -z 10485760  -c -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
 		#
 		## gzip with pigz compression
+		#
 		echo "Creation partclone image for partition with  ${LABEL} label with "${FILE_SYSTEM}" filesystem and pigz compression tool"
-		partclone.$FILE_SYSTEM -N -z 10485760  -c -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
+		partclone.$FILE_SYSTEM  -N  -z 10485760  -c -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
 		#
 	fi
-	echo "################################################################################################################"
-	echo "################################################################################################################"
-	echo "################################################################################################################"
-	chown 1000:1000 "${FILE_NAME}"
+	chown 1000:1000 *.gz
 done
-################################################################################################################
+##############################################################################################################################################################################
 ## BACKUP of GPT Partition table
 ## MAIN DISK
 #
@@ -110,40 +86,4 @@ sync
 chown 1000:1000 *_table.backup
 chown 1000:1000 *_dump.txt
 #
-################################################################################################################
-#
-## Backup LVM metadata configuration (OPTIONAL)
-#
-# MAIN_CRYPT_DEV="/dev/disk/by-label/${MAIN_CRYPT_LABEL}"
-# SECOND_CRYPT_DEV="/dev/disk/by-label/${SECOND_CRYPT_LABEL}"
-# LVM_DEVICE="/dev/mapper/${LVM_DECRYPTED_NAME}"
-#
-## backup PARTUUID and UUIDs of LVM and encrypted partitions (OPTIONAL)
-# echo "Backup PARTUUIDs and UUID for LUKS/LVM..."
-# blkid -s PARTUUID -o value "${MAIN_CRYPT_DEV}" > "${MAIN_CRYPT_LABEL}_PARTUUID.txt"
-# blkid -s PARTUUID -o value "${SECOND_CRYPT_DEV}" > "${SECOND_CRYPT_LABEL}_PARTUUID.txt"
-# blkid -s UUID -o value "${MAIN_CRYPT_DEV}" > "${MAIN_CRYPT_LABEL}_UUID.txt"
-# blkid -s UUID -o value "${SECOND_CRYPT_DEV}" > "${SECOND_CRYPT_LABEL}_UUID.txt"
-# blkid -s UUID -o value "${LVM_DEVICE}" > "${LVM_DECRYPTED_NAME}_UUID.txt"
-# chown 1000:1000 *.txt
-#
-## backup luks headers (OPTIONAL)
-#
-# echo "Backup LUKS header for main encrypted partition..."
-# cryptsetup luksHeaderBackup "${MAIN_CRYPT_DEV}" --header-backup-file "${MAIN_CRYPT_LABEL}_header_backup"
-# gpg --symmetric --output "${MAIN_CRYPT_LABEL}_header_backup.gpg" "${MAIN_CRYPT_LABEL}_header_backup"
-# shred --iterations=3 --zero --remove=wipesync "${MAIN_CRYPT_LABEL}_header_backup"
-#
-# echo "Backup LUKS header for second encrypted partition..."
-# cryptsetup luksHeaderBackup "${SECOND_CRYPT_DEV}" --header-backup-file "${SECOND_CRYPT_LABEL}_header_backup"
-# gpg --symmetric --output "${SECOND_CRYPT_LABEL}_header_backup.gpg" "${SECOND_CRYPT_LABEL}_header_backup"
-# shred --iterations=3 --zero --remove=wipesync "${SECOND_CRYPT_LABEL}_header_backup"
-# chown 1000:1000 *.gpg
-#
-## backup volume group metadata (OPTIONAL)
-#
-# echo "Backup volume group metadata..."
-# vgcfgbackup -f "${VG_NAME}_METADATA.backup" "${VG_NAME}"
-# chown 1000:1000 "${VG_NAME}_METADATA.backup"
-#
-################################################################################################################
+##############################################################################################################################################################################
--- RESTORE.sh
+++ RESTORE.sh
@@ -1,46 +1,18 @@
 #!/bin/bash
+##############################################################################################################################################################################
 #
-## DISKs devices names found in "/dev/disk/by-id/*"
-# MAIN_DISK_NAME="nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M910268J"
-# SECOND_DISK_NAME="nvme-Samsung_SSD_980_1TB_S649NJ0R214022Y"
+## Partitions for backup
 #
-## PARTITIONS LABELS
 declare -A PARTITIONS=(["EFI"]="vfat" ["WINMSR"]="raw" ["WINOS"]="ntfs" ["WINREC"]="ntfs" ["WINDATA"]="ntfs" )
 #
-## Restore partition table (OPTIONAL)
-# MAIN_DISK="/dev/disk/by-id/${MAIN_DISK_NAME}"
-# dd if="${MAIN_DISK_NAME}_table.backup" of="${MAIN_DISK}" bs=512 status=progress
-# sync
-# gdisk "${MAIN_DISK}" 
-#
-## Get password for encryption ( optional )
-# PASS_CRED=$(gpg --quiet --decrypt credentials.gpg)
-#
+##############################################################################################################################################################################
 ## MAIN 
-################################################################################################################
-## OPTIONAL restore partitions table with dd
 #
-# MAIN_DISK="/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M910268J"
-# TABLE_BACKUP_FILE_NAME="$(basename ${MAIN_DISK})_table.backup"
-# dd if="${TABLE_BACKUP_FILE_NAME}" of="${MAIN_DISK}" bs=512 status=progress && sync
-#
-################################################################################################################
-## OPTIONAL restore LVM_information
-################################################################################################################
-#
-# LVM_DEVICE="/dev/nvme1n1p6"
-# VG_NAME="vg"
-# UUID="xyz"
-# pvcreate --uuid "${UUID}" --norestorefile "${LVM_DEVICE}"
-# vgcfgrestore -f vg_backup "${VG_NAME}"
-#
-################################################################################################################
 for PART in "${!PARTITIONS[@]}"; 
 do
+	echo "################################################################################################################"
 	LABEL="${PART}"
-	echo "Current partition label: ${LABEL}"
 	FILE_SYSTEM="${PARTITIONS[$LABEL]}"
-	echo "Current partition filesystem: ${FILE_SYSTEM}"
 	echo "Processing partition with ${LABEL} label and ${FILE_SYSTEM} filesystem"
 	PARTLABEL_DEV="/dev/disk/by-partlabel/${LABEL}"
 	if [[ -h "${PARTLABEL_DEV}" ]];
@@ -58,46 +30,33 @@ do
 			continue
 		fi
 	fi
-	## 7z compression
-	# FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.7z"
 	#
 	## gzip compression(gzip/pigz)
+	#
 	FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.gz"
 	#
 	if [[ -f ${FILE_NAME} ]];
 	then
 		if [ "${FILE_SYSTEM}" = "raw" ];
 		then
-			## 7z compression
-			# echo "Restoring raw image for partition with $LABEL label."
-			# 7z x -so "${FILE_NAME}" | partclone.dd -N -z 10485760 --source - -o "${DEV_PATH}"
-			#
-			## 7z compression and encryption
-			# echo "Restoring raw image for partition with $LABEL label."
-			# 7z x -so -p"${PASS_CRED}" "${FILE_NAME}" | partclone.dd -N -z 10485760 --source - -o "${DEV_PATH}"
-			#
 			## gzip compression
+			# 
 			# echo "Restoring raw image for $LABEL."
 			# gzip -c -d "${FILE_NAME}" | partclone.dd -N -z 10485760 --source - -o "${DEV_PATH}"
 			#
 			## gzip with pigz compression
+			#
 			echo "Restoring raw image for partition with $LABEL label."
 			pigz -d -c "${FILE_NAME}" | partclone.dd -N -z 10485760 --source - -o "${DEV_PATH}"
 			#
 		else
-			## 7z compression
-			# echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
-			# 7z x -so "${FILE_NAME}" | partclone.$FILE_SYSTEM -N -z 10485760 --source - -r -o  "${DEV_PATH}"
-			#
-			## 7z compression and encryption
-			# echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
-			# 7z x -so -p"${PASS_CRED}"  "${FILE_NAME}" | partclone.$FILE_SYSTEM -N -z 10485760 --source - -r -o  "${DEV_PATH}"
-			#
 			## gzip compression
+			#
 			# echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
 			# gzip -c -d "${FILE_NAME}" | partclone.$FILE_SYSTEM -N-z 10485760 --source - -r -o  "${DEV_PATH}"
 			#
 			## gzip with pigz compression
+			#
 			echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
 			pigz -d -c "${FILE_NAME}" | partclone.$FILE_SYSTEM -N -z 10485760 --source - -r -o  "${DEV_PATH}"
 			#
@@ -107,6 +66,5 @@ do
 		continue
 	fi
 	echo "################################################################################################################"
-	echo "################################################################################################################"
-	echo "################################################################################################################"
 done
+##############################################################################################################################################################################
