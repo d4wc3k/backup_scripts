9826c83f66d91548ddf6d1bc9e3c475af30e2058
d4wc4k <115703675+d4wc3k@users.noreply.github.com>
2025-10-22 08:12:08 +0000

    Add files via upload

--- BACKUP.sh
+++ BACKUP.sh
@@ -3,11 +3,13 @@
 #
 ## DISKs devices names found in "/dev/disk/by-id/*"
 MAIN_DISK_NAME="nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M910268J"
-# SECOND_DISK_NAME="nvme-Samsung_SSD_980_1TB_S649NJ0R214022Y"
+SECOND_DISK_NAME="nvme-Samsung_SSD_980_1TB_S649NJ0R214022Y"
 #
 ## Partitions for backup
-#
+# PARTLABEL 
 declare -A PARTITIONS=(["EFI"]="vfat" ["WINMSR"]="raw" ["WINOS"]="ntfs" ["WINREC"]="ntfs" ["WINDATA"]="ntfs" )
+# PARTLABEL + LABEL
+# declare -A PARTITIONS=(["EFI"]="vfat" ["WINMSR"]="raw" ["WinOS"]="ntfs" ["WinRec"]="ntfs" ["WinData"]="ntfs" )
 #
 ##############################################################################################################################################################################
 ## MAIN
@@ -37,53 +39,83 @@ do
 	#
 	## gzip compression(gzip/pigz)
 	#
-	FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.gz"
+	# FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.gz"
+	#
+	## 7zip compression
+	#
+	# FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.7z"
+	#
+	## rar compression
+	#
+	FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.rar"
 	#
 	if [ "${FILE_SYSTEM}" = "raw" ];
 	then
 		## gzip compression
 		#
 		# echo "Creation raw image for partition with  ${LABEL} label with gz compression tool"
-		# partclone.dd -N -z 10485760  -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
+		# partclone.dd -z 10485760 -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
 		#
 		## gzip with pigz compression
 		#
-		echo "Creation raw image for partition with  ${LABEL} label with pigz compression tool"
-		partclone.dd -N -z 10485760  -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
+		# echo "Creation raw image for partition with  ${LABEL} label with pigz compression tool"
+		# partclone.dd -z 10485760 -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
+		#
+		## 7zip compression
+		#
+		# echo "Creation raw image for partition with ${LABEL} label with 7zip compression tool"
+		# partclone.dd -z 10485760  -s "${DEV_PATH}" --output - | 7z a -bd -t7z "${FILE_NAME}" -si -m0=lzma2 -mx=3 -mmt8
+		#
+		## rar compression
+		#
+		echo "Creation raw image for partition with ${LABEL} label with rar compression tool"
+		partclone.dd -z 10485760  -s "${DEV_PATH}" --output - | rar a -idq -k -m2 -md32m -mt16 -rr30 -si"${LABEL}.img" "${FILE_NAME}"
 		#
 	else
 		## gzip compression
 		#
 		# echo "Creation partclone image for partition with  ${LABEL} label with gz compression tool"
-		# partclone.$FILE_SYSTEM -N -z 10485760  -c -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
+		# partclone.$FILE_SYSTEM -z 10485760 -c -s "${DEV_PATH}" --output - | gzip -c -6 > "${FILE_NAME}"
 		#
 		## gzip with pigz compression
 		#
-		echo "Creation partclone image for partition with  ${LABEL} label with "${FILE_SYSTEM}" filesystem and pigz compression tool"
-		partclone.$FILE_SYSTEM  -N  -z 10485760  -c -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
+		# echo "Creation partclone image for partition with  ${LABEL} label with "${FILE_SYSTEM}" filesystem and pigz compression tool"
+		# partclone.$FILE_SYSTEM -z 10485760 -c -s "${DEV_PATH}" --output - | pigz -c --fast -b 1024 --rsyncable > "${FILE_NAME}"
+		#
+		## 7zip compression
+		#
+		# echo "Creation partclone image for partition with  ${LABEL} label with "${FILE_SYSTEM}" filesystem and 7zip compression tool"
+		# partclone.$FILE_SYSTEM -z 10485760  -c -s "${DEV_PATH}" --output - | 7z a -bd -t7z "${FILE_NAME}" -si -m0=lzma2 -mx=3 -mmt8
+		#
+		## rar compression
+		echo "Creation partclone image for partition with  ${LABEL} label with "${FILE_SYSTEM}" filesystem and rar compression tool"
+		partclone.$FILE_SYSTEM -z 10485760  -c -s "${DEV_PATH}" --output - | rar a -idq -k -m2 -md32m -mt16 -rr30  -si"${LABEL}.img" "${FILE_NAME}"
 		#
 	fi
-	chown 1000:1000 *.gz
+	# chown 1000:1000 *.gz
+	# chown 1000:1000 *.7z
+	chown 1000:1000 *.rar
 done
 ##############################################################################################################################################################################
 ## BACKUP of GPT Partition table
 ## MAIN DISK
 #
+echo "################################################################################################################"
 echo "Backup main partition table..."
 MAIN_DISK="/dev/disk/by-id/${MAIN_DISK_NAME}"
-dd if=${MAIN_DISK} of="${MAIN_DISK_NAME}_table.backup" bs=512 count=34 status=progress
-sfdisk --dump "${MAIN_DISK}" > "${MAIN_DISK_NAME}_dump.txt"
+dd if=${MAIN_DISK} of="main_table.bin" bs=512 count=34 status=progress && sync
+sfdisk --dump "${MAIN_DISK}" > "main_dump.txt"
 #
 ## SECOND DISK
 #
-# echo "Backup second partition table..."
-# SECOND_DISK="/dev/disk/by-id/${SECOND_DISK_NAME}"
-# dd if=${SECOND_DISK} of="${SECOND_DISK_NAME}_table.backup" bs=512 count=34 status=progress
-# sfdisk --dump "${SECOND_DISK}" > "${SECOND_DISK_NAME}_dump.txt"
-## 
-echo "Syncing..."
-sync
-chown 1000:1000 *_table.backup
+echo "################################################################################################################"
+echo "Backup second partition table..."
+SECOND_DISK="/dev/disk/by-id/${SECOND_DISK_NAME}"
+dd if=${SECOND_DISK} of="second_table.bin" bs=512 count=34 status=progress && sync
+sfdisk --dump "${SECOND_DISK}" > "second_dump.txt"
+##
+chown 1000:1000 *_table.bin
 chown 1000:1000 *_dump.txt
+echo "################################################################################################################"
 #
 ##############################################################################################################################################################################
--- RESTORE.sh
+++ RESTORE.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 ##############################################################################################################################################################################
+## Restoring partition tables
+## Uncomment if there is needed recreation of disk layouts
+#
+# MAIN_DISK_NAME="nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M910268J"
+# MAIN_DISK="/dev/disk/by-id/${MAIN_DISK_NAME}"
+# dd if="main_table.bin" of="${MAIN_DISK}" bs=512 status=progress && sync
+# gdisk "${MAIN_DISK}"
+#
+# SECOND_DISK_NAME="nvme-Samsung_SSD_980_1TB_S649NJ0R214022Y"
+# SECOND_DISK="/dev/disk/by-id/${SECOND_DISK_NAME}"
+# dd if="second_table.bin" of="${SECOND_DISK}" bs=512 status=progress && sync
+# gdisk "${SECOND_DISK}"
+##############################################################################################################################################################################
 #
 ## Partitions for backup
 #
+# PARTLABEL
 declare -A PARTITIONS=(["EFI"]="vfat" ["WINMSR"]="raw" ["WINOS"]="ntfs" ["WINREC"]="ntfs" ["WINDATA"]="ntfs" )
+# PARTLABEL + LABEL
+# declare -A PARTITIONS=(["EFI"]="vfat" ["WINMSR"]="raw" ["WinOS"]="ntfs" ["WinRec"]="ntfs" ["WinData"]="ntfs" )
 #
 ##############################################################################################################################################################################
 ## MAIN 
@@ -33,7 +49,14 @@ do
 	#
 	## gzip compression(gzip/pigz)
 	#
-	FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.gz"
+	# FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.gz"
+	#
+	## 7zip compression
+	#
+	# FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.7z"
+	#
+	## rar compression
+	FILE_NAME="$(echo ${LABEL} | tr '[:upper:]' '[:lower:]').img.rar"
 	#
 	if [[ -f ${FILE_NAME} ]];
 	then
@@ -42,23 +65,43 @@ do
 			## gzip compression
 			# 
 			# echo "Restoring raw image for $LABEL."
-			# gzip -c -d "${FILE_NAME}" | partclone.dd -N -z 10485760 --source - -o "${DEV_PATH}"
+			# gzip -c -d "${FILE_NAME}" | partclone.dd -z 10485760 --source - -o "${DEV_PATH}"
 			#
 			## gzip with pigz compression
 			#
+			# echo "Restoring raw image for partition with $LABEL label."
+			# pigz -d -c "${FILE_NAME}" | partclone.dd -z 10485760 --source - -o "${DEV_PATH}"
+			#
+			## 7zip compression
+			#
+			# echo "Restoring raw image for partition with $LABEL label."
+			# 7z x -bd -so "${FILE_NAME}" | partclone.dd -z 10485760 --source - -o "${DEV_PATH}"
+			#
+			## rar compression
+			#
 			echo "Restoring raw image for partition with $LABEL label."
-			pigz -d -c "${FILE_NAME}" | partclone.dd -N -z 10485760 --source - -o "${DEV_PATH}"
+			rar p "${FILE_NAME}" | partclone.dd -z 10485760 --source - -o "${DEV_PATH}"
 			#
 		else
 			## gzip compression
 			#
 			# echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
-			# gzip -c -d "${FILE_NAME}" | partclone.$FILE_SYSTEM -N-z 10485760 --source - -r -o  "${DEV_PATH}"
+			# gzip -c -d "${FILE_NAME}" | partclone.$FILE_SYSTEM -z 10485760 --source - -r -o  "${DEV_PATH}"
 			#
 			## gzip with pigz compression
 			#
+			# echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
+			# pigz -d -c "${FILE_NAME}" | partclone.$FILE_SYSTEM -z 10485760 --source - -r -o  "${DEV_PATH}"
+			#
+			## 7zip compression
+			#
+			# echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
+			# 7z x -bd -so "${FILE_NAME}" | partclone.$FILE_SYSTEM -z 10485760 --source - -r -o  "${DEV_PATH}"
+			#
+			## rar compression
+			#
 			echo "Restoring partclone image for partition with $LABEL label and ${FILE_SYSTEM} filesystem."
-			pigz -d -c "${FILE_NAME}" | partclone.$FILE_SYSTEM -N -z 10485760 --source - -r -o  "${DEV_PATH}"
+			rar p "${FILE_NAME}" | partclone.$FILE_SYSTEM -z 10485760 --source - -r -o  "${DEV_PATH}"
 			#
 		fi
 	else
